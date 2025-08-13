c81b5fe6cb129da3f2c4f6848861a20932ce73d9
이제희 <103947888+pinkkj@users.noreply.github.com>
2025-08-14 05:07:29 +0900

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
 
-#SBATCH -J ksy-love
+#SBATCH srun
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-gpu=8
-#SBATCH --mem-per-gpu=32G
-#SBATCH -p batch_ugrad
-#SBATCH -t 1-00:00:00
+#SBATCH --mem-per-gpu=10G
+#SBATCH -p debug_ugrad
+#SBATCH -w aurora-g3
+#SBATCH --pty $SHELL
 #SBATCH -o logs/slurm-%A.out
 
 python app.py
