d714a08d9430f7d408536fbc159ae53adff2346f
이제희 <103947888+pinkkj@users.noreply.github.com>
2025-08-14 05:01:23 +0900

    Update app.sh

--- app.sh
+++ app.sh
@@ -7,14 +7,5 @@
 #SBATCH -p batch_ugrad
 #SBATCH -t 1-00:00:00
 #SBATCH -o logs/slurm-%A.out
-set -euo pipefail
 
-PY="/data/juventa23/anaconda3/envs/kb_env/bin/python"
-
-echo "HOST: $HOSTNAME"
-echo "PWD : $(pwd)"
-echo "Using PY: $PY"
-"$PY" -V
-
-# train.py가 없으면 app.py로 바꾸세요
-"$PY" -u app.py
+python app.py
